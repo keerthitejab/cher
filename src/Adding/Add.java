a669a30afcc3086d5ab2d14e958987b631070c62
keerthitejab <keerthitejabodapati85@gmail.com>
2024-11-11 19:52:33 +0530

    this is my first commit

---
+++ src/Adding/Add.java
@@ -0,0 +1,17 @@
+package Adding;
+
+public class Add {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=15;
+		
+		int c= a+b;
+		
+		System.out.println("addition of two numbers is: " +c);
+
+		
+	}
+
+}
